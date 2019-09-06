9eff26994c41bc494f33c641519d77f1701258a6
Marcelo Tosco @capynet <marcelo@front.id>
2019-09-07 01:13:17 +0200

    Add some code documentation.

--- src/components/filter-at-column/filter-at-column.vue
+++ src/components/filter-at-column/filter-at-column.vue
@@ -1,50 +1,34 @@
 <template>
+    <v-data-table :headers="headers" :items="desserts" item-key="name" class="elevation-1 pa-6">
+        <template v-slot:top>
 
-    <v-container>
-        <v-row>
-            <v-col cols="12">
+            <!-- v-container, v-col and v-row are just for decoration purposes. -->
+            <v-container fluid>
+                <v-row>
 
+                    <v-col cols="6">
+                        <v-row class="pa-6">
+                            <!-- Filter for dessert name-->
+                            <v-text-field v-model="dessertFilterValue" type="text" label="Name"></v-text-field>
+                        </v-row>
+                    </v-col>
 
-                <v-data-table
-                        :headers="headers"
-                        :items="desserts"
-                        item-key="name"
-                        class="elevation-1 pa-6"
-                >
-                    <template v-slot:top>
+                    <v-col cols="6">
+                        <v-row class="pa-6">
+                            <!-- Filter for calories -->
+                            <v-select
+                                    :items="caloriesList"
+                                    v-model="caloriesFilterValue"
+                                    label="Calories"
+                            ></v-select>
+                        </v-row>
+                    </v-col>
 
+                </v-row>
+            </v-container>
 
-                        <v-container fluid>
-                            <v-row>
-
-                                <v-col cols="6">
-                                    <v-row class="pa-6">
-                                        <!-- Filter for dessert name-->
-                                        <v-text-field v-model="dessertFilterValue" type="text" label="Name"></v-text-field>
-                                    </v-row>
-                                </v-col>
-
-                                <v-col cols="6">
-                                    <v-row class="pa-6">
-                                        <!-- Filter for calories -->
-                                        <v-select
-                                                :items="caloriesList"
-                                                v-model="caloriesFilterValue"
-                                                label="Calories"
-                                        ></v-select>
-                                    </v-row>
-                                </v-col>
-
-                            </v-row>
-                        </v-container>
-
-                    </template>
-                </v-data-table>
-
-
-            </v-col>
-        </v-row>
-    </v-container>
+        </template>
+    </v-data-table>
 </template>
 
 <script>
@@ -77,30 +61,12 @@
             align: 'left',
             sortable: false,
             value: 'name',
-            filter: (value) => {
-              // If this filter has no value we just skip the entire filter.
-              if (!this.dessertFilterValue) {
-                return true;
-              }
-
-              // Check if the current loop value (The dessert name)
-              // partially contains the searched word.
-              return value.toLowerCase().includes(this.dessertFilterValue.toLowerCase());
-            },
+            filter: this.nameFilter,
           },
           {
             text: 'Calories',
             value: 'calories',
-            filter: (value) => {
-              // If this filter has no value we just skip the entire filter.
-              if (!this.caloriesFilterValue) {
-                return true;
-              }
-
-              // Check if the current loop value (The calories value)
-              // equals to the selected value at the <v-select>.
-              return value === this.caloriesFilterValue;
-            },
+            filter: this.caloriesFilter,
           },
           {text: 'Fat (g)', value: 'fat'},
           {text: 'Carbs (g)', value: 'carbs'},
@@ -108,6 +74,39 @@
           {text: 'Iron (%)', value: 'iron'},
         ]
       },
+    },
+    methods: {
+      /**
+       * Filter for dessert names column.
+       * @param value Value to be tested.
+       * @returns {boolean}
+       */
+      nameFilter(value) {
+        // If this filter has no value we just skip the entire filter.
+        if (!this.dessertFilterValue) {
+          return true;
+        }
+
+        // Check if the current loop value (The dessert name)
+        // partially contains the searched word.
+        return value.toLowerCase().includes(this.dessertFilterValue.toLowerCase());
+      },
+
+      /**
+       * Filter for calories column.
+       * @param value Value to be tested.
+       * @returns {boolean}
+       */
+      caloriesFilter(value) {
+        // If this filter has no value we just skip the entire filter.
+        if (!this.caloriesFilterValue) {
+          return true;
+        }
+
+        // Check if the current loop value (The calories value)
+        // equals to the selected value at the <v-select>.
+        return value === this.caloriesFilterValue;
+      }
     }
   }
 </script>
